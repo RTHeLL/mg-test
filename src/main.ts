e015457091fdf2529ab57c9df14b8df6cb744886
Kinder <k1ndermail@ya.ru>
2024-03-24 20:27:22 +0300

    feat(code): add swagger;

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app/app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+async function createSwaggerDocument(app: INestApplication<any>) {
+  const config = new DocumentBuilder()
+    .setTitle('MarketGuru Test')
+    .setDescription('MarketGuru Test API description')
+    .setVersion('1.0')
+    .addTag('MG API')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const server_port = configService.get('server_port');
+
+  await createSwaggerDocument(app);
+
   await app.listen(server_port);
 }
 
